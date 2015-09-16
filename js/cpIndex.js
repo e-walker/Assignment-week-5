d232d91c6bcadd3088488a5f5130d08f91da5617
Emily Walker <emilycwalker@gmail.com>
2015-09-16 21:04:27 +1000

    mostly working

--- js/cpIndex.js
+++ js/cpIndex.js
@@ -1,39 +1,35 @@
 $(document).ready(function() {
     console.log( "ready" );
 
-var city = $('#city-type').val().toLowerCase();
+	function changeDisplay() {
+		var city = $('#city-type').val().toLowerCase();
 
-console.log("city");
+		console.log("city");
 
-//$('#submit-btn').click(changeDisplay);
+		if (city === 'nyc' || city === 'new york city' || city === 'new york') {
+			$('body').css('background-image', 'url(images/nyc.jpg)');
+		}
 
-$('#submit-btn').submit(function() {
-	return false;
-})
+		else if (city === 'sf' || city === 'san francisco' || city === 'bay area') {
+			$('body').css('background-image', 'url(images/sf.jpg)');
+		}
 
-function changeDisplay () {
+		else if (city === 'la' || city === 'los angeles' || city === 'lax') {
+			$('body').css('background-image', 'url(../images/la.jpg)');
+		}
 
-if (city === 'nyc' || city === 'new york city' || city === 'new york') {
-	$('body').css('background-image', 'url(../images/nyc.jpg)');
-}
+		else if (city === 'austin' || city === 'atx') {
+			$('body').css('background-image', 'url(../images/austin.jpg)');
+		}
 
-else if (city === 'sf' || city === 'san francisco' || city === 'bay area') {
-	$('body').css('background-image', 'url(../images/sf.jpg)');
-}
+		else if (city === 'sydney' || city === 'syd') {
+			$('body').css('background-image', 'url(../images/sydney.jpg)');
+		}
 
-else if (city === 'la' || city === 'los angeles' || city === 'lax') {
-	$('body').css('background-image', 'url(../images/la.jpg)');
-}
+		return false;
+	}
 
-else if (city === 'austin' || city === 'atx') {
-	$('body').css('background-image', 'url(../images/austin.jpg)');
-}
-
-else if (city === 'sydney' || city === 'syd') {
-	$('body').css('background-image', 'url(../images/sydney.jpg)');
-}
-
-}
+	$('#searchForm').submit(changeDisplay);
 
 
 });
