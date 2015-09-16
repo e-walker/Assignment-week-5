2319fc07c1174960669f8ddc389471dbb572c550
Emily Walker <emilycwalker@gmail.com>
2015-09-16 20:46:57 +1000

    on submit return false

--- js/cpIndex.js
+++ js/cpIndex.js
@@ -1,37 +1,39 @@
 $(document).ready(function() {
     console.log( "ready" );
 
-
 var city = $('#city-type').val().toLowerCase();
 
-console.log("city")
+console.log("city");
+
+//$('#submit-btn').click(changeDisplay);
+
+$('#submit-btn').submit(function() {
+	return false;
+})
+
+function changeDisplay () {
 
 if (city === 'nyc' || city === 'new york city' || city === 'new york') {
 	$('body').css('background-image', 'url(../images/nyc.jpg)');
 }
 
 else if (city === 'sf' || city === 'san francisco' || city === 'bay area') {
-
-
+	$('body').css('background-image', 'url(../images/sf.jpg)');
 }
 
 else if (city === 'la' || city === 'los angeles' || city === 'lax') {
-
-
+	$('body').css('background-image', 'url(../images/la.jpg)');
 }
 
-
 else if (city === 'austin' || city === 'atx') {
-
-
+	$('body').css('background-image', 'url(../images/austin.jpg)');
 }
 
 else if (city === 'sydney' || city === 'syd') {
-
-
+	$('body').css('background-image', 'url(../images/sydney.jpg)');
 }
 
-
+}
 
 
 });
